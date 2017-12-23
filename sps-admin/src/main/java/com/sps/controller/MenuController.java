a7f5e47c3f0afd1226b7b064a710c08cfb4d502c
juzi-yangningsheng <354434673@qq.com>
2017-12-23 15:40:01 +0800

    删除了文件

--- sps-admin/src/main/java/com/sps/controller/MenuController.java
+++ sps-admin/src/main/java/com/sps/controller/MenuController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.shiro.SecurityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,7 +15,8 @@ import com.alibaba.fastjson.JSON;
 import com.sps.dao.user.SpsMenuMapper;
 import com.sps.entity.user.SpsMenu;
 import com.sps.entity.user.SpsMenuExample;
-import com.sps.service.MenuService;
+import com.sps.entity.user.SpsUser;
+import com.sps.service.user.MenuService;
 import com.sps.util.MenuUtil;
 
 @Controller
@@ -25,7 +27,8 @@ public class MenuController {
 
 	@RequestMapping(value = "/getMenu.json")
 	public @ResponseBody String getMenuList(){
-	
+		String userName = (String) SecurityUtils.getSubject().getPrincipal();
+		
 		HashMap<String, List<MenuUtil>> menu = menuService.getMenu();
 		
 		String jsonString = JSON.toJSONString(menu);
