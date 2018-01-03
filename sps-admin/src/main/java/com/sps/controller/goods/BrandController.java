4666bc35951258ee38257bda62676d5dbcd5c6ad
杨宁盛 <yangningsheng@juzifenqi.com>
2018-01-03 19:43:09 +0800

    Update BrandController.java

--- sps-admin/src/main/java/com/sps/controller/goods/BrandController.java
+++ sps-admin/src/main/java/com/sps/controller/goods/BrandController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.sps.entity.goods.SpsBrand;
-import org.sps.service.TestDubbo;
 import org.sps.service.goods.BrandService;
 
 import java.util.HashMap;
