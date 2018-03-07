63191945299be2fd1cd6c767f7a7d9607a1c5534
liucailing <liucailing@juzifenqi.com>
2018-03-07 18:47:35 +0800

    帮助与反馈接口完成

---
+++ sps-api/src/main/java/com/sps/service/app/help/HelpService.java
@@ -0,0 +1,31 @@
+package com.sps.service.app.help;
+
+import com.sps.entity.app.Help;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018-03-06.
+ *
+ * @projectName ${project_name}.
+ * @Package ${package_name}.
+ * @ClassName ${type_name}.
+ * @Description description  the function of the class
+ * @Author 刘彩玲
+ * @createDate ${date}$ ${timme}$
+ */
+public interface HelpService {
+    /**
+     * 获取所有的标题
+     * @return List
+     */
+    List<Help>  queryAllTittle();
+
+    /**
+     * 根据id，查询内容信息
+     * @param date
+     * @return
+     */
+    HashMap<String, Object> queryContent(String date);
+}
