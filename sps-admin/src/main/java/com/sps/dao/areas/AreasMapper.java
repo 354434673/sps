e02b19e5b73191a99aec3b9365c46c6a3afafa5f
sunchao <sunchao@juzifenqi.com>
2018-03-22 17:58:25 +0800

    sunchao-pom文件

--- sps-admin/src/main/java/com/sps/dao/areas/AreasMapper.java
+++ sps-admin/src/main/java/com/sps/dao/areas/AreasMapper.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.Param;
 
 import com.sps.entity.areas.Areas;
 import com.sps.entity.areas.AreasExample;
-import com.sps.entity.areas.AreasVo;
 
 public interface AreasMapper {
     int countByExample(AreasExample example);
