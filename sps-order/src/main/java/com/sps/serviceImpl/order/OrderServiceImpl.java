55b02a5a6c5c16bc56d91aa53ea2f0ac41dee541
juzi-yangningsheng <354434673@qq.com>
2018-03-28 21:13:34 +0800

    Signed-off-by: juzi-yangningsheng <354434673@qq.com>

--- sps-order/src/main/java/com/sps/serviceImpl/order/OrderServiceImpl.java
+++ sps-order/src/main/java/com/sps/serviceImpl/order/OrderServiceImpl.java
@@ -8,8 +8,6 @@ import com.alibaba.fastjson.JSONObject;
 import com.sps.dao.order.OrderRepayDetailMapper;
 import com.sps.util.HttpClientUtil;
 import com.sps.util.HttpClientUtils;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
