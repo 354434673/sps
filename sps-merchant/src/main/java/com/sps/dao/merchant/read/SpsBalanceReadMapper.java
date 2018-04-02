d95fb687c2565ed577be5c0cab48472199c3dd55
juzi-yangningsheng <354434673@qq.com>
2018-04-02 14:59:33 +0800

    更新代码

---
+++ sps-merchant/src/main/java/com/sps/dao/merchant/read/SpsBalanceReadMapper.java
@@ -0,0 +1,23 @@
+package com.sps.dao.merchant.read;
+
+
+import org.apache.ibatis.annotations.Param;
+import org.sps.entity.merchant.SpsChannelBalance;
+/**
+ * Created by Administrator on 2018-03-14.
+ *
+ * @projectName ${project_name}.
+ * @Package ${package_name}.
+ * @ClassName ${type_name}.
+ * @Description description  the function of the class
+ * @Author 刘彩玲
+ * @createDate ${date}$ ${timme}$
+ */
+public interface SpsBalanceReadMapper {
+    /**
+     *
+     * @param userNo
+     * @return
+     */
+    SpsChannelBalance selectByUserId(@Param("userNo") String userNo);
+}
---
+++ sps-merchant/src/main/java/org/sps/entity/merchant/SpsChannelBalance.java
@@ -0,0 +1,65 @@
+package org.sps.entity.merchant;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+/**
+ * Created by Administrator on 2018-03-14.
+ *
+ * @projectName ${project_name}.
+ * @Package ${package_name}.
+ * @ClassName ${type_name}.
+ * @Description description  the function of the class
+ * @Author 刘彩玲
+ * @createDate ${date}$ ${timme}$
+ */
+public class SpsChannelBalance  implements Serializable{
+//    主键id
+    private Integer id;
+//    余额
+    private BigDecimal balance;
+//    外键用户id
+    private  String userNo;
+//    用户类型
+    private Integer userType;
+//    创建时间
+    private Date createTime;
+//    更新时间
+    private Date  updateTime;
+//get与set方法
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public BigDecimal getBalance() {
+        return balance;
+    }
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+    public String getUserNo() {
+        return userNo;
+    }
+    public void setUserNo(String userNo) {
+        this.userNo = userNo;
+    }
+    public Integer getUserType() {
+        return userType;
+    }
+    public void setUserType(Integer userType) {
+        this.userType = userType;
+    }
+    public Date getCreateTime() {
+        return createTime;
+    }
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
